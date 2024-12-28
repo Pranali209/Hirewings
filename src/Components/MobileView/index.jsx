28a300d2f6da59afc9c7f67ec7329e7b0cd33152
Pranali <pgmodgekar99@gmail.com>
2024-12-28 11:59:41 +0530

    top fix

--- src/Components/MobileView/index.jsx
+++ src/Components/MobileView/index.jsx
@@ -52,7 +52,7 @@ function MobileView() {
             txtsize="text-[8px]"
           />
         </div>
-        <img src={FallingLine} className=' absolute right-[8.25rem] top-[39.9rem]' />
+        <img src={FallingLine} className=' absolute right-[8.25rem] top-[40.15rem]' />
       </section>
 
       <div className='  max-md:flex flex-col lg:hidden items-center max-md:mt-10'>
